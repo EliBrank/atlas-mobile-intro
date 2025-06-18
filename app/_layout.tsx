d9a92b827284270b3b4a3bf53e826319b03f3fe4
EliBrank <eliasbrinkman@gmail.com>
2025-06-18 17:13:09 -0500

    create database provider hook for db

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,19 @@
+import { DatabaseProvider } from "@/components/DatabaseProvider";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="add-activity-screen"
-        options={{ headerShown: false }}
-      />
-    </Stack>
+    <DatabaseProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="add-activity-screen"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+    </DatabaseProvider>
   );
 }
